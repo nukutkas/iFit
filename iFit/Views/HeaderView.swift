91788692e39ac893bc4b0243bdfa96f83ab292cd
Nikita Kochetov <65461320+nukutkas@users.noreply.github.com>
2022-10-20 11:19:25 +0300

    Buttons have been added.

--- iFit/Views/HeaderView.swift
+++ iFit/Views/HeaderView.swift
@@ -31,44 +31,33 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-import AVKit
 
-struct ExerciseView: View {
-    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-    let index: Int
-    let interval: TimeInterval = 30
-    
+struct HeaderView: View {
+    let exerciseName: String
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                HeaderView(exerciseName: exerciseNames[index])
-                    .padding(.bottom)
-                if let url = Bundle.main.url(
-                  forResource: videoNames[index],
-                  withExtension: "mp4") {
-                    VideoPlayer(player: AVPlayer(url: url))
-                        .frame(height: geometry.size.height * 0.45)
-                } else {
-                    Text("Couldn’t find \(videoNames[index]).mp4")
-                      .foregroundColor(.red)
-                }
-                Text(Date().addingTimeInterval(interval), style: .timer)
-                  .font(.system(size: 90))
-                Button("Start/Done") { }
-                  .font(.title3)
-                  .padding()
-                Text("Rating")
-                Spacer()
-                Button("History") { }
-                  .padding(.bottom)
+        VStack {
+            Text(exerciseName)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
             }
         }
     }
 }
 
-struct ExerciseView_Previews: PreviewProvider {
+
+struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView(index: 0)
+        Group {
+            HeaderView(exerciseName: "Squat")
+                .previewLayout(.sizeThatFits)
+            HeaderView(exerciseName: "Squat")
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityLarge)
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
