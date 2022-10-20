fa07962dd5d65b20a868cea1e550c219e4ee947d
Nikita Kochetov <65461320+nukutkas@users.noreply.github.com>
2022-10-20 12:29:01 +0300

    .resizedToFill has been added instead of three modifiers.

--- iFit/Views/WelcomeView.swift
+++ iFit/Views/WelcomeView.swift
@@ -44,10 +44,11 @@ struct WelcomeView: View {
                     .font(.headline)
                 }
                   Image("step-up")
-                      .resizable()
-                      .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                      .frame(width: 240.0, height: 240.0)
-                      .clipShape(Circle())
+//                      .resizable()
+//                      .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+//                      .frame(width: 240.0, height: 240.0)
+//                      .clipShape(Circle())
+                        .resizedToFill(width: 240, height: 240)
               }
             }
             VStack {
