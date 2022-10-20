09e07fcdb948b7d51030e4316ea384072bba9ba3
Nikita Kochetov <65461320+nukutkas@users.noreply.github.com>
2022-10-20 12:25:14 +0300

    WelcomeView has been redesigned.

--- iFit/Views/WelcomeView.swift
+++ iFit/Views/WelcomeView.swift
@@ -34,7 +34,29 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack(alignment: .bottom) {
+                VStack(alignment: .leading) {
+                  Text("Get fit")
+                    .font(.largeTitle)
+                  Text("with high intensity interval training")
+                    .font(.headline)
+                }
+                  Image("step-up")
+                      .resizable()
+                      .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                      .frame(width: 240.0, height: 240.0)
+                      .clipShape(Circle())
+              }
+            }
+            VStack {
+                HeaderView(titleText: "Welcome")
+                Spacer()
+                Button("History") { }
+                  .padding(.bottom)
+            }
+        }
     }
 }
 
